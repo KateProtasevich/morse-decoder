a940ad2a967ca879f4872353341cd23e82be1b2f
KateProtasevich <KrishniovaKate@mail.ru>
2020-03-04 17:56:15 +0300

    init: start Morse task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let i = 0;
+  let letter = "";
+  let symbol = "";
+  let result = "";
+  let letterStr = "";
+
+  while (i <expr.length) {
+    while (letterStr.length < 10 & expr[i]!= "*" & expr[i]!= "") {
+      letterStr = letterStr + expr[i];
+      i++;
+    }
+    /*
+        while (letterStr.length < 10) {
+          Доработать: добавление нулей в начале у короткого слова
+         }
+    */
+  let j = 0;
+  while (j < 10) {
+    switch (letterStr.substr(j,2)) {
+        case "00": symbol = "";break;
+        case "10": symbol = ".";break;
+        case "11": symbol = "-";break;
+      }
+    j = j+2;
+    letter = letter + symbol;
+    symbol = "";
+  }
+
+  if (MORSE_TABLE.hasOwnProperty(letter)) {
+    result = result + MORSE_TABLE[letter];
+  }
+  letter = "";
+  letterStr = "";
+
+  if (expr[i]== "*") {
+      i=i+10;
+      result = result + " ";
+    }
+  }
+  return result;
 }
 
 module.exports = {
